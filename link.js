cfa6f1dd82c28b0d0c5d72d6a621eb4d23316f3a
ajoyab95 <130991585+ajoyab95@users.noreply.github.com>
2023-04-25 18:11:57 +0600

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,1 @@
-const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=testtoken=https://test.link.com&html_redirect=1 ";
+const offerLink = "vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqbTBJVkxNMjJLMTUzMVI2Y05Cd3Q1VW43dndQUXxBQ3Jtc0ttWnZCQkMxT0pZOTRucE5lcjE3Und4RTZmSGR4N3JGdmRBR29VZWtUOExyUDRnZkhMamZuQXJyRnl0aXdfVmVZYlpsNk82RlRYTDdVSno2TzlvZENsZEd5dUZ5YnlhbHpGZ3pPS0RkTVBhbjVwdE1oZw&q=https%3A%2F%2Fxblroo.paiatialdates.net%2F%3Futm_source%3Dda57dc555e50572d%26s1%3D187579%26s2%3D1785913%26j1%3D1&html_redirect=1";
